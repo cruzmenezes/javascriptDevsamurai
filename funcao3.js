871b6220221a9b269030ad101a0f23de958a3b46
Edson Victor da cruz Menezes <91231848+cruzmenezes@users.noreply.github.com>
2023-07-18 15:53:16 +0000

    função return

---
+++ funcao3.js
@@ -0,0 +1,31 @@
+function name(name, lastName, age){
+  
+    console.log(`Ola, tudo bem, seja bem vindo!!! ${name} ${lastName} vc ainda esta com ${age} anos..`)
+  }
+  
+  name("Edson", "menezes", 42);
+  
+  
+  function sum(n1, n2){
+    console.log(10 + 2)
+  }
+  
+  sum();
+    
+  
+  function sum2(n1,n2,n3,n4){
+    console.log('A média do aluno é', (10 + 5 + 6 + 8) / 4)
+    
+  }  
+  
+  sum2();
+  
+  
+  
+  function sum3 (n1, n2){
+    
+    return (n1 * n2) / 2
+    
+  }
+  
+  console.log(`O resultado é: ${sum3(10, 3)}`);
